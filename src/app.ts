60481c6c7b7cdd0b708dee2a7296279be5933362
naimuddin94 <developernaimuddin@gmail.com>
2024-11-02 14:51:49 +0600

    middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import routes from "./app/routes";
 
 const app: Application = express();
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send({ message: "PH Health Care Server is running :(" });
 });
+
+app.use("/api/v1", routes);
 
 export default app;
