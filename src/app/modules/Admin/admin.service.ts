aee93eebcbf5e531e3f302fd957e8ef2f005d466
naimuddin94 <developernaimuddin@gmail.com>
2024-11-05 21:01:16 +0600

    update admin.service.ts

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,11 +1,15 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma } from "@prisma/client";
+import { pagination, TOptions } from "../../helper/pagination";
+import prisma from "../../helper/prisma";
+import { adminSearchableFields } from "./admin.constant";
 
-const prisma = new PrismaClient();
-
-const adminSearchableFields = ["name", "email", "contactNumber"];
-
-const fetchAllAdminFromDB = async (query: any) => {
+const fetchAllAdminFromDB = async (
+  query: Record<string, unknown>,
+  options: TOptions
+) => {
   const { searchTerm, ...filteredValues } = query;
+
+  const { page, skip, limit } = pagination.calculatePagination(options);
 
   const andCondition: Prisma.AdminWhereInput[] = [];
 
@@ -31,9 +35,26 @@ const fetchAllAdminFromDB = async (query: any) => {
   }
 
   const whereCondition: Prisma.AdminWhereInput = { AND: andCondition };
-  return await prisma.admin.findMany({
+
+  const data = await prisma.admin.findMany({
+    where: whereCondition,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: "desc",
+          },
+  });
+
+  const total = await prisma.admin.count({
     where: whereCondition,
   });
+
+  return { data, meta: { page, limit, total } };
 };
 
 export const AdminService = {
