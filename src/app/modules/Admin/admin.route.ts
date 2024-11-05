98dd7aa69910c21e02378b8f364a15ec9af60617
naimuddin94 <developernaimuddin@gmail.com>
2024-11-05 15:29:16 +0600

    feat: create admin.route.ts

---
+++ src/app/modules/Admin/admin.route.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import { AdminController } from "./admin.controller";
+
+const router = express.Router();
+
+router.route("/").get(AdminController.getAllAdmin);
+
+export const adminRoutes = router;
