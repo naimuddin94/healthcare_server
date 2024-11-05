ff8061494bbb4c5d50a948eabafc776e444e7cfd
naimuddin94 <developernaimuddin@gmail.com>
2024-11-05 21:01:09 +0600

    feat: create admin.constant.ts

---
+++ src/app/modules/Admin/admin.constant.ts
@@ -0,0 +1,10 @@
+export const adminFilterableFields = [
+  "name",
+  "email",
+  "contactNumber",
+  "searchTerm",
+];
+
+export const adminSearchableFields = ["name", "email", "contactNumber"];
+
+export const adminOptionFields = ["page", "limit", "sortBy", "sortOrder"];
