32dc30e49cdfccc30ab196ee2dbf1110685b4f42
naimuddin94 <developernaimuddin@gmail.com>
2024-11-08 19:50:22 +0600

    update some issue

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import routes from "./app/routes";
 import globalErrorHandler from "./app/utils/globalErrorHandler";
 
@@ -17,5 +17,16 @@ app.use("/api/v1", routes);
 
 //@ts-ignore
 app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({
+    success: false,
+    message: "API not found!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested API endpoint not found!",
+    },
+  });
+});
 
 export default app;
