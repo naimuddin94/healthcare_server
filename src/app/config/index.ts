66b4c1f3f6bea1819974334deb9cb5ad367a2beb
naimuddin94 <developernaimuddin@gmail.com>
2024-11-15 09:11:45 +0600

    update src/app/config/index.ts

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,4 +1,6 @@
 export const config = {
   access_secret: process.env.ACCESS_TOKEN_SECRET,
   refresh_secret: process.env.REFRESH_TOKEN_SECRET,
+  access_token_expire: process.env.ACCESS_TOKEN_EXPIRY,
+  refresh_token_expire: process.env.REFRESH_TOKEN_EXPIRY,
 };
