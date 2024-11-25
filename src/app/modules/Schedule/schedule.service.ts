065223d2be654f854db285419e904dd63faced7b
naimuddin94 <developernaimuddin@gmail.com>
2024-11-25 15:14:47 +0600

    schedule slot create  logic

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,34 +1,41 @@
-import { addHours, format } from "date-fns";
+import { addHours, addMinutes, format } from "date-fns";
 
 const insertIntoDB = async (payload: any) => {
-    const { startDate, endDate, startTime, endTime } = payload;
-    
-    console.log({ startDate, endDate, startTime, endTime });
+  const { startDate, endDate, startTime, endTime } = payload;
+
+  const intervalTime = 30;
 
   const currentDate = new Date(startDate);
-    const lastDate = new Date(endDate);
-    
-    console.log({currentDate, lastDate});
+  const lastDate = new Date(endDate);
 
-//   while (currentDate <= lastDate) {
-//     const startDateTime = new Date(
-//       addHours(
-//         `${format(currentDate, "yyyy-MM-dd")}`,
-//         Number(startTime.split(":")[0])
-//       )
-//     );
+  while (currentDate <= lastDate) {
+    const startDateTime = new Date(
+      addHours(
+        `${format(currentDate, "yyyy-MM-dd")}`,
+        Number(startTime.split(":")[0])
+      )
+    );
 
-//     const endDateTime = new Date(
-//       addHours(
-//         `${format(lastDate, "yyyy-MM-dd")}`,
-//         Number(endTime.split(":")[0])
-//       )
-//     );
+    const endDateTime = new Date(
+      addHours(
+        `${format(currentDate, "yyyy-MM-dd")}`,
+        Number(endTime.split(":")[0])
+      )
+    );
 
-//     while (startDateTime <= endDateTime) {
-//       // next module.
-//     }
-//   }
+    while (startDateTime < endDateTime) {
+      const scheduleData = {
+        startDateTime: startDateTime,
+        endDateTime: addMinutes(startDateTime, intervalTime),
+      };
+
+      console.log({ scheduleData });
+
+      startDateTime.setMinutes(startDateTime.getMinutes() + intervalTime);
+    }
+
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
 };
 
 export const ScheduleService = {
